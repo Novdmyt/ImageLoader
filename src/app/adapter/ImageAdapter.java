ffcace316d9133974c97b81cfd7948238f271d26
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-08-31 21:35:38 +0300

    Override method image

--- src/app/adapter/ImageAdapter.java
+++ src/app/adapter/ImageAdapter.java
@@ -8,7 +8,7 @@ public class ImageAdapter implements ImageViewer {
 
     Viewer viewer;
 
-    public ImageAdapter(String fileType){
+    public ImageAdapter(String fileType) {
         if (fileType.equalsIgnoreCase("pdf")) {
             viewer = new PdfViewer();
         } else if (fileType.equalsIgnoreCase("png")) {
@@ -16,5 +16,12 @@ public class ImageAdapter implements ImageViewer {
         }
     }
 
-
+    @Override
+    public void image(String typeFile, String nameFile) {
+        if (typeFile.equalsIgnoreCase("pdf")) {
+            viewer.viewerPdf(nameFile);
+        } else if (typeFile.equalsIgnoreCase("png")) {
+            viewer.viewerPng(nameFile);
+        }
+    }
 }
